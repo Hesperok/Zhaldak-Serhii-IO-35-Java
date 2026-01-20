08f2b8c5a7224868c3add1ae1a4eadfd065751a5
Hesperok <jaldak1983@gmail.com>
2026-01-20 16:55:54 +0200

    Add Punctuation class to represent punctuation marks

---
+++ Lab4/Punctuation.java
@@ -0,0 +1,15 @@
+/**
+ * Клас, що представляє розділовий знак.
+ */
+public class Punctuation {
+
+    private final char symbol;
+
+    public Punctuation(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
